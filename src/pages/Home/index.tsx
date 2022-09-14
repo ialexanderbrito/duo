feeccc167a20085c14a9abbf9891ea4d684cd189
ialexanderbrito <xanderzinho26@gmail.com>
2022-09-14 18:57:04 -0300

    feat: add page initial

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,117 @@
-import { useTheme } from 'contexts/Theme';
-
-import styles from './Home.module.scss';
+import logoImg from 'assets/logo-esports.svg';
+import { MagnifyingGlassPlus } from 'phosphor-react';
 
 export function Homepage() {
-  const { theme } = useTheme();
-
   return (
     <>
-      <div className={styles.home} data-theme={theme}>
-        <h1>
-          vite<span>boilerplate 🤙🏾</span>
+      <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+        <img src={logoImg} alt="eSports" />
+
+        <h1 className="text-6xl text-white font-black mt-20">
+          Seu{' '}
+          <span className="bg-nlw-gradient bg-clip-text text-transparent">
+            duo
+          </span>{' '}
+          está aqui
         </h1>
+
+        <div className="grid-cols-6 grid gap-6 mt-16">
+          <a href="" className="relative rounded-lg overflow-hidden">
+            <img
+              src="https://static-cdn.jtvnw.net/ttv-boxart/21779-188x250.jpg"
+              alt="Jogo"
+            />
+
+            <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+              <strong className="font-bold block text-white">
+                League of Legends
+              </strong>
+              <span className="text-zinc-300 text-sm block">4 anuncios</span>
+            </div>
+          </a>
+          <a href="" className="relative rounded-lg overflow-hidden">
+            <img
+              src="https://static-cdn.jtvnw.net/ttv-boxart/21779-188x250.jpg"
+              alt="Jogo"
+            />
+
+            <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+              <strong className="font-bold block text-white">
+                League of Legends
+              </strong>
+              <span className="text-zinc-300 text-sm block">4 anuncios</span>
+            </div>
+          </a>
+          <a href="" className="relative rounded-lg overflow-hidden">
+            <img
+              src="https://static-cdn.jtvnw.net/ttv-boxart/21779-188x250.jpg"
+              alt="Jogo"
+            />
+
+            <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+              <strong className="font-bold block text-white">
+                League of Legends
+              </strong>
+              <span className="text-zinc-300 text-sm block">4 anuncios</span>
+            </div>
+          </a>
+          <a href="" className="relative rounded-lg overflow-hidden">
+            <img
+              src="https://static-cdn.jtvnw.net/ttv-boxart/21779-188x250.jpg"
+              alt="Jogo"
+            />
+
+            <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+              <strong className="font-bold block text-white">
+                League of Legends
+              </strong>
+              <span className="text-zinc-300 text-sm block">4 anuncios</span>
+            </div>
+          </a>
+          <a href="" className="relative rounded-lg overflow-hidden">
+            <img
+              src="https://static-cdn.jtvnw.net/ttv-boxart/21779-188x250.jpg"
+              alt="Jogo"
+            />
+
+            <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+              <strong className="font-bold block text-white">
+                League of Legends
+              </strong>
+              <span className="text-zinc-300 text-sm block">4 anuncios</span>
+            </div>
+          </a>
+          <a href="" className="relative rounded-lg overflow-hidden">
+            <img
+              src="https://static-cdn.jtvnw.net/ttv-boxart/21779-188x250.jpg"
+              alt="Jogo"
+            />
+
+            <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+              <strong className="font-bold block text-white">
+                League of Legends
+              </strong>
+              <span className="text-zinc-300 text-sm block">4 anuncios</span>
+            </div>
+          </a>
+        </div>
+
+        <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8">
+          <div className="bg-[#2a2634] px-8 py-6 flex justify-between items-center">
+            <div>
+              <strong className="text-2xl text-white font-black block">
+                Não escontrou seu duo?
+              </strong>
+              <span className="text-zinc-400 block">
+                Publique um anúncio para encontrar novos players!
+              </span>
+            </div>
+            <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+              <MagnifyingGlassPlus size={24} />
+              Publicar anúncio
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
