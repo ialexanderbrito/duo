8760e6187d3f7c72aeff1f074583ff76c642a858
ialexanderbrito <xanderzinho26@gmail.com>
2022-09-17 18:08:06 -0300

    feat: add route game ads

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 
+import { GamedAd } from 'pages/GameAd';
 import { Homepage } from 'pages/Home';
 
 export function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="game/:id" element={<GamedAd />} />
     </Routes>
   );
 }
