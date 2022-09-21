62c13ddb74a03387a4cae2dcd9d1f712957a187d
ialexanderbrito <xanderzinho26@gmail.com>
2022-09-20 21:07:54 -0300

    fix: user banner discord modal

--- src/components/UserBanner/index.tsx
+++ src/components/UserBanner/index.tsx
@@ -1,6 +1,16 @@
+import { useState } from 'react';
+
+import * as Dialog from '@radix-ui/react-dialog';
 import { GameController } from 'phosphor-react';
 
+import { DiscordModal } from 'components/DiscordModal';
+
+import { useToast } from 'contexts/Toast';
+
+import { getDiscordId } from 'services/get/games';
+
 interface UserBannerProps {
+  id: string;
   name: string;
   weekDays: string[];
   useVoiceChannel: boolean;
@@ -10,49 +20,74 @@ interface UserBannerProps {
 }
 
 export function UserBanner(props: UserBannerProps) {
+  const { toast } = useToast();
+
+  const [discord, setDiscord] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function loadDiscord(id: string) {
+    try {
+      const { data } = await getDiscordId(id);
+
+      setDiscord(data.discord);
+
+      setIsLoading(false);
+    } catch (error) {
+      toast.error('Erro ao carregar o Discord');
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
-    <div className="keen-slider__slide flex flex-col bg-[#2A2634] w-72 h-96 relative rounded-lg overflow-hidden flex-shrink-0 p-8">
-      <div className="mb-4">
-        <p className="text-zinc-500">Nome</p>
+    <Dialog.Root>
+      <div className="keen-slider__slide flex flex-col bg-[#2A2634] w-72 h-96 relative rounded-lg overflow-hidden flex-shrink-0 p-8">
+        <div className="mb-4">
+          <p className="text-zinc-500">Nome</p>
 
-        <strong className="text-white">{props.name}</strong>
+          <strong className="text-white">{props.name}</strong>
+        </div>
+
+        <div className="mb-4">
+          <p className="text-zinc-500">Tempo de jogo</p>
+
+          <strong className="text-white">{props.yearsPlaying} ano(s)</strong>
+        </div>
+
+        <div className="mb-4">
+          <p className="text-zinc-500">Disponibilidade</p>
+
+          <strong className="text-white">
+            {props.weekDays.length} dias・{props.hourStart}h - {props.hourEnd}h
+          </strong>
+        </div>
+
+        <div className="mb-4">
+          <p className="text-zinc-500">Chamada de áudio?</p>
+
+          <strong
+            className={
+              props.useVoiceChannel ? 'text-emerald-500' : 'text-red-500'
+            }
+          >
+            {props.useVoiceChannel ? 'Sim' : 'Não'}
+          </strong>
+        </div>
+
+        <div className="mt-4 flex items-center justify-center">
+          <Dialog.Trigger
+            className="bg-violet-500 w-full px-5 h-12 justify-center rounded-md font-semibold flex items-center gap-3 text-white hover:bg-violet-600"
+            onClick={() => {
+              loadDiscord(props.id);
+            }}
+          >
+            <GameController size={24} />
+            Conectar
+          </Dialog.Trigger>
+        </div>
       </div>
 
-      <div className="mb-4">
-        <p className="text-zinc-500">Tempo de jogo</p>
-
-        <strong className="text-white">{props.yearsPlaying} ano(s)</strong>
-      </div>
-
-      <div className="mb-4">
-        <p className="text-zinc-500">Disponibilidade</p>
-
-        <strong className="text-white">
-          {props.weekDays.length} dias・{props.hourStart}h - {props.hourEnd}h
-        </strong>
-      </div>
-
-      <div className="mb-4">
-        <p className="text-zinc-500">Chamada de áudio?</p>
-
-        <strong
-          className={
-            props.useVoiceChannel ? 'text-emerald-500' : 'text-red-500'
-          }
-        >
-          {props.useVoiceChannel ? 'Sim' : 'Não'}
-        </strong>
-      </div>
-
-      <div className="mt-4 flex items-center justify-center">
-        <button
-          className="bg-violet-500 w-full px-5 h-12 justify-center rounded-md font-semibold flex items-center gap-3 text-white hover:bg-violet-600"
-          type="submit"
-        >
-          <GameController size={24} />
-          Conectar
-        </button>
-      </div>
-    </div>
+      <DiscordModal discord={discord} loading={isLoading} />
+    </Dialog.Root>
   );
 }
